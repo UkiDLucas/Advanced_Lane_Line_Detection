73cbd8329ec2956ec80a75030df86992b6c977a6
Uki D. Lucas <UkiDLucas@gmail.com>
2017-03-26 22:31:44 -0500

    add functionality that creates markdown files

---
+++ convert_notebooks.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[1]:
+
+# see http://nbconvert.readthedocs.io/en/latest/usage.html
+get_ipython().system('jupyter nbconvert --to markdown camera_calibration.ipynb')
+
+
+# In[2]:
+
+# see http://nbconvert.readthedocs.io/en/latest/usage.html
+get_ipython().system('jupyter nbconvert --to markdown README.ipynb')
+
+
+# In[ ]:
+
+
+
