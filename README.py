d5300d001b4703054dcd3250e1db98b6e0d69eab
Uki D. Lucas <UkiDLucas@gmail.com>
2017-04-11 08:07:04 -0500

    removed class yada-yada

--- README.py
+++ README.py
@@ -385,38 +385,6 @@ plot_with_corners(image_warped, DEST, inline=True)
 
 
 
-# ### 3. Perspective transform to bird-eye-view
-# 
-# #### OpenCV function or other method has been used to correctly rectify each image to a "birds-eye view". Transformed images should be included in the writeup (or saved to a folder) and submitted with the project.
-# 
-# The code for my perspective transform includes a function called warper(), which appears in lines 1 through 8 in the file example.py (output_images/examples/example.py) (or, for example, in the 3rd code cell of the IPython notebook).  The warper() function takes as inputs an image (img), as well as source (src) and destination (dst) points.  I chose the hardcode the source and destination points in the following manner:
-# 
-#     src = np.float32(
-#         [[(img_size[0] / 2) - 55, img_size[1] / 2 + 100],
-#         [((img_size[0] / 6) - 10), img_size[1]],
-#         [(img_size[0] * 5 / 6) + 60, img_size[1]],
-#         [(img_size[0] / 2 + 55), img_size[1] / 2 + 100]])
-#     dst = np.float32(
-#         [[(img_size[0] / 4), 0],
-#         [(img_size[0] / 4), img_size[1]],
-#         [(img_size[0] * 3 / 4), img_size[1]],
-#         [(img_size[0] * 3 / 4), 0]])
-# 
-# This resulted in the following source and destination points:
-# 
-#    Source  	Destination
-#   585, 460 	  320, 0   
-#   203, 720 	 320, 720  
-#   1127, 720	 960, 720  
-#   695, 460 	  960, 0   
-# 
-# I verified that my perspective transform was working as expected by drawing the src and dst points onto a test image and its warped counterpart to verify that the lines appear parallel in the warped image.
-# 
-# 
-# 
-# Road Transformed
-# <img src="test_images/test1.jpg" alt="Road Transformed" />
-
 # ### 4. Identified lane-line pixels and fit their positions with a polynomial
 # 
 # #### Methods have been used to identify lane line pixels in the rectified binary image. The left and right line have been identified and fit with a curved functional form (e.g., spine or polynomial). Example images with line pixels identified and a fit overplotted should be included in the writeup (or saved to a folder) and submitted with the project.
